5e9786c357f7be90d796dfd2a40e24975aec4639
Zack Hanni <zack.hannI@gmail.com>
2023-08-08 13:28:42 -0400

    format css for sample result

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
-
 
 function App() {
   return (
-    <div className="App">
-        <h1>hello world</h1>
+    <div className="app">
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>London</p>
+            </div>
+            <div className="temp">
+              <h2>65 degrees</h2>
+            </div>
+            <div className="description">
+              <p>Cloudy with a chance of code</p>
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              <p className="bold">65 degrees</p>
+              <p>Feeks Like</p>
+            </div>
+            <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+            </div>
+            <div className="wind">
+            <p className="bold">12 MPH</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
